c5f7e7808857c1ad210c4d991e324c5fc1a9cd0b
rootoor-dev <rootoor.projects@gmail.com>
2022-05-13 12:23:53 +0000

    Update JavaMatrixMultiplication.java

--- src/java/JavaMatrixMultiplication.java
+++ src/java/JavaMatrixMultiplication.java
@@ -20,4 +20,213 @@ public class Main
                         System.out.println("");
                 }
 	}
+	
+	
+	
+	/**********************************************************************\
+				METHODES DE MATRICE
+	\**********************************************************************/
+	
+	public static String toCSV(String[] array) {
+        String result = "";
+
+        if (array.length > 0) {
+            StringBuilder sb = new StringBuilder();
+
+            for (String s : array) {
+                sb.append(s).append(",");
+            }
+
+            result = sb.deleteCharAt(sb.length() - 1).toString();
+        }
+        return result;
+}
+
+
+ static void printMatrix(int[][] matrix) {
+        for (int[] line : matrix) {
+            int i = 0;
+            StringBuilder sb = new StringBuilder(matrix.length);
+            for (int number : line) {
+                if (i != 0) {
+                    sb.append("\t");
+                } else {
+                    i++;
+                }
+                sb.append(number);
+            }
+            System.out.println(sb.toString());
+        }
+    }
+	
+	
+static void printMatrix(Matrix matrix, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j != 0) {
+                    System.out.print("\t");
+                }
+                System.out.printf("%.0f", matrix.get(i, j));
+            }
+            System.out.println("");
+        }
+    }
+	
+	
+ private static int[][] add(int[][] A, int[][] B) {
+        int n = A.length;
+        int[][] C = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        return C;
+    }
+
+    private static int[][] subtract(int[][] A, int[][] B) {
+        int n = A.length;
+        int[][] C = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = A[i][j] - B[i][j];
+            }
+        }
+        return C;
+    }	
+	
+	
+public static double[][] mult(double a[][], double b[][]){//a[m][n], b[n][p]
+   if(a.length == 0) return new double[0][0];
+   if(a[0].length != b.length) return null; //invalid dims
+ 
+   int n = a[0].length;
+   int m = a.length;
+   int p = b[0].length;
+ 
+   double ans[][] = new double[m][p];
+ 
+   for(int i = 0;i < m;i++){
+      for(int j = 0;j < p;j++){
+         for(int k = 0;k < n;k++){
+            ans[i][j] += a[i][k] * b[k][j];
+         }
+      }
+   }
+   return ans;
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
